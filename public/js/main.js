b3f1c4f95d65c18ceb9cdf9d69cd1772afa8cd1f
Nikrajput <nikrajput8383@gmail.com>
2021-05-12 20:47:09 +0530

    fixing error

--- public/js/main.js
+++ public/js/main.js
@@ -34,7 +34,7 @@ const getInfo=async(event)=>{
     }
     else{
         try{
-            let url=`http://api.openweathermap.org/data/2.5/weather?q=${cityName.value}&units=metric&appid=5f00240edd73620e48bf2bc65e908ad2`
+            let url=`https://api.openweathermap.org/data/2.5/weather?q=${cityName.value}&units=metric&appid=5f00240edd73620e48bf2bc65e908ad2`
             const response=await fetch(url)
             const data=[await response.json()]
             city.innerText=`${data[0].name}, ${data[0].sys.country}`
